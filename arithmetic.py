fc8fe45e54ed54f926c85d17df6aa1d7828fe544
ishikanimmagadda <ishikan84@gmail.com>
2024-06-16 15:47:16 -0500

    1)arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+def arithmetic (num1, num2): 
+    addition = num1 + num2 
+    subtraction = num1 - num2
+    multiplication = num1 * num2
+    division = num1/num2
+
+    print (addition, subtraction, multiplication, division)
+
+arithmetic(5,6)
+arithmetic(8,2)
+
